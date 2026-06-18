54a3484f7b8d1d75ea35fa9637de5f4ea72ad1fb
MURILO DIETHEMBERG TOFANIN <murilo.tofanin@portalsesisp.org.br>
2026-06-18 13:46:37 -0300

    Fiz a primeira função que é a de adicionar produtos

---
+++ Estoque.py
@@ -0,0 +1,9 @@
+
+##Tive ajuda do Miguel para fazer esse codigo e tambem do Kauan
+
+estoque = [
+     [1,"Volante", 2, "Prateleira01"],
+     [2,"Sapato", 4, "Prateleira02"],
+     [3,"Ecapamento", 5, "Prateleira03"],
+     [4,"Capô", 8, "Prateleira04"],
+     ]
