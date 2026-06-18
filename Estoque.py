7e2d3a7e491ea6f77131587e175c785921ea874a
MURILO DIETHEMBERG TOFANIN <murilo.tofanin@portalsesisp.org.br>
2026-06-18 14:15:23 -0300

    Adicionei a função de listar produtos

--- Estoque.py
+++ Estoque.py
@@ -7,3 +7,31 @@ estoque = [
      [3,"Ecapamento", 5, "Prateleira03"],
      [4,"Capô", 8, "Prateleira04"],
      ]
+
+
+proximoID = 5
+
+
+def adicionarProdutos():
+        global proximoID   
+        id_item = int(input("Digite o ID: "))
+        nome = input("Digite o nome: ")
+        quantidade = int(input("Digite a quantidade: "))
+        localizacao = input("Digite a localização: ")
+
+        produto = [id_item, nome, quantidade, localizacao]
+
+        estoque.append(produto)
+        proximoID = proximoID + 1
+
+        print("Produto adicionado!👍👍")
+
+
+def listarprodutos():
+      print("Mostrando os produtos da lista!!")
+      for i in estoque:
+            print(i)
+
+        
+        
+        
